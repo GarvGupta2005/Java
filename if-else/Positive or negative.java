0b414d0bdfaff63245ae5432de0da09b89e833b8
GarvGupta2005 <garvg310@gmail.com>
2025-06-21 23:11:43 +0530

    Create Positive or negative.java

---
+++ if-else/Positive or negative.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int num = 7;
+    if (num > 0) {
+      System.out.println("Number is Positive");
+    } 
+    else {
+      System.out.println("Number is Negative");
+    }
+  }
+}
