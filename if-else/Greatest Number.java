b1de468c88cfaadf937ec2dce9ca5126f3ada96a
GarvGupta2005 <garvg310@gmail.com>
2025-06-21 23:17:53 +0530

    Create Greatest Number.java

---
+++ if-else/Greatest Number.java
@@ -0,0 +1,21 @@
+public class Main{
+  public static void main(String[] args){
+    int Num1 = 54;
+    int Num2 = 34;
+    if(Num1>Num2){
+      System.out.println("Num1 is Greatest");
+    }
+    else{
+      System.out.println("Num2 is Greatest");
+    }
+  }
+}
+
+
+public class Main{
+  public static void main(String[] args){
+    int Num1 = 54;
+    int Num2 = 34;
+    System.out.println(Math.max(Num1,Num2));
+  }
+}
