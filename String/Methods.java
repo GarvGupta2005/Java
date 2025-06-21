0667204ec5a755ab745114d6cac31bf809738c47
GarvGupta2005 <garvg310@gmail.com>
2025-06-21 22:43:48 +0530

    Create Methods.java

---
+++ String/Methods.java
@@ -0,0 +1,7 @@
+//Length
+public class Main {
+  public static void main(String[] args) {
+    String greet = "Good Morning";
+    System.out.print("Length of greet is : " + greet.length());
+  }
+}
