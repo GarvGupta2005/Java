1c29b2ebdd09d73c30b2b157acf8e0d8d61965c6
GarvGupta2005 <garvg310@gmail.com>
2025-06-21 22:45:58 +0530

    Update Methods.java

--- String/Methods.java
+++ String/Methods.java
@@ -1,7 +1,26 @@
-//Length
+// Length
 public class Main {
   public static void main(String[] args) {
     String greet = "Good Morning";
     System.out.print("Length of greet is : " + greet.length());
+  }
+}
+
+
+// upercase and lowercase
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Hello World";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
+
+
+// finding character
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Please locate where 'locate' occurs!";
+    System.out.println(txt.indexOf("locate"));
   }
 }
