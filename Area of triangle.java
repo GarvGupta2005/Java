58acaa03eec67bff789061700de1e89b81a0de4b
GarvGupta2005 <garvg310@gmail.com>
2025-06-21 18:32:06 +0530

    Update Area of triangle.java

--- Area of triangle.java
+++ Area of triangle.java
@@ -1,14 +1,10 @@
-public class Main {
-  public static void main(String[] args) {
-    // Create integer variables
-    int length = 4;
-    int width = 6;
-    int area;
-
-    area = length * width;
-
-    System.out.println("Length is: " + length);
-    System.out.println("Width is: " + width);
-    System.out.println("Area of the rectangle is: " + area);
-  }
-}
+public class Main{
+	public static void main(String[] args){
+    
+    int length = 15;
+    int breadth = 10;
+    
+    int Area = length*breadth;
+    System.out.print("Area: "+ Area);
+    }
+}    
