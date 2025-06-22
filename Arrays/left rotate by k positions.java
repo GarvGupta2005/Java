152f6eeedf128183fe9af3bc2e06fb3e8742faea
GarvGupta2005 <garvg310@gmail.com>
2025-06-22 22:40:39 +0530

    Left rotate by k positions.java

--- Arrays/left rotate by k positions.java
+++ Arrays/left rotate by k positions.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        
+        System.out.print("enter size: ");
         int size = sc.nextInt();
-
         int[] arr = new int[size];
-
-        for (int i = 0; i < size; i++) {
+        for(int i = 0; i<size; i++){
+            System.out.println("enter element");
             arr[i] = sc.nextInt();
         }
-
+         
+        System.out.print("Enter k: "); 
         int k = sc.nextInt();
-        k = k % size; 
-
+        k = k%size;
+        
         int[] temp = new int[size];
-
-        for (int j = 0; j < size - k; j++) {
-            temp[j] = arr[k + j];
+        for(int j = 0; j<size-k; j++){
+            temp[j] = arr[j+k];
         }
-
-        for (int z = size - k; z < size; z++) {
-            temp[z] = arr[z - (size - k)];
+        //temp = [3 4 5]
+        //size-k = 3
+        for(int z = size-k; z<size; z++){
+            temp[z] = arr[z-(size-k)];
         }
-
-        for (int i = 0; i < size; i++) {
-            System.out.print(temp[i] + " ");
+        
+        System.out.print("rotated array: ");
+        for(int b = 0; b<size; b++){
+            System.out.print(temp[b]+" ");
         }
     }
 }
