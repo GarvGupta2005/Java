1da978c3016c38cadd0f6660ac79680461376d52
GarvGupta2005 <garvg310@gmail.com>
2025-06-24 18:11:17 +0530

    Methods.java

---
+++ DSA/Set/Methods.java
@@ -0,0 +1,6 @@
+// HashSet  TreeSet  LinkedHashSet
+add()	      Adds an element if it's not already in the set
+remove()	  Removes the element from the set
+contains()	Checks if the set contains the element
+size()	    Returns the number of elements
+clear()	    Removes all elements
