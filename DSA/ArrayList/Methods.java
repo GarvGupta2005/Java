0c9fa2fa673ce39437b903429779f9f76b401457
GarvGupta2005 <garvg310@gmail.com>
2025-06-24 17:51:28 +0530

    Methods.java

--- DSA/ArrayList/Methods.java
+++ DSA/ArrayList/Methods.java
@@ -1,5 +1,7 @@
-add()	Adds an element to the end of the list
-get()	Returns the element at the specified position
-set()	Replaces the element at the specified position
+add() Adds an element to the end of the list
+get() Returns the element at the specified position
+set() Replaces the element at the specified position
 remove() Removes the element at the specified position
-size()	Returns the number of elements in the list
+size() Returns the number of elements in the list
+clear() Remove all elements in list
+sort() sorting lists alphabetically or numerically  
