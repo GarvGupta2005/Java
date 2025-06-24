43875af908a496f40b39d0996e6db86b87d6ca54
GarvGupta2005 <garvg310@gmail.com>
2025-06-24 17:57:06 +0530

    Update Methods.java

--- DSA/ArrayList/Methods.java
+++ DSA/ArrayList/Methods.java
@@ -1,17 +1,17 @@
 //ArrrayList
-add() Adds an element to the end of the list
-get() Returns the element at the specified position
-set() Replaces the element at the specified position
+add()    Adds an element to the end of the list
+get()    Returns the element at the specified position
+set()    Replaces the element at the specified position
 remove() Removes the element at the specified position
-size() Returns the number of elements in the list
-clear() Remove all elements in list
-sort() sorting lists alphabetically or numerically  
+size()   Returns the number of elements in the list
+clear()  Remove all elements in list
+sort()   Sorting lists alphabetically or numerically  
 
 
 //LinkedList
-addFirst()	Adds an element to the beginning of the list	
-addLast()	Add an element to the end of the list	
+addFirst()	  Adds an element to the beginning of the list	
+addLast()	    Add an element to the end of the list	
 removeFirst()	Remove an element from the beginning of the list	
 removeLast()	Remove an element from the end of the list	
-getFirst()	Get the element at the beginning of the list	
-getLast()	Get the element at the end of the list  
+getFirst()	  Get the element at the beginning of the list	
+getLast()	    Get the element at the end of the list  
