c72c73184056f4bdadcb4f32be1e667330092853
GarvGupta2005 <garvg310@gmail.com>
2025-06-24 17:43:29 +0530

    Methods.java

---
+++ DSA/ArrayList/Methods.java
@@ -0,0 +1,5 @@
+add()	Adds an element to the end of the list
+get()	Returns the element at the specified position
+set()	Replaces the element at the specified position
+remove()	Removes the element at the specified position
+size()	Returns the number of elements in the list
