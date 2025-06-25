bd64fcc7c6ac94437681204fbc6e88dc743632eb
GarvGupta2005 <garvg310@gmail.com>
2025-06-25 22:07:34 +0530

    Update Methods.java

--- String/Methods.java
+++ String/Methods.java
@@ -35,3 +35,49 @@ public class Main {
   }
 }
 // if we add string and number output will be string
+
+
+// chatAt the specified index
+public class Main {
+  public static void main(String[] args) {
+    String myStr = "Hello";
+    char result = myStr.charAt(0);
+    System.out.println(result);
+  }
+}
+
+
+//compareTo() compares two strings
+public class Main {
+  public static void main(String[] args) {
+    String myStr1 = "Hello";
+    String myStr2 = "Hello";
+    System.out.println(myStr1.compareTo(myStr2)); // Returns 0 because they are equal
+  }
+}
+
+
+//contains()
+public class Main {
+  public static void main(String[] args) {
+    String myStr = "Hello";
+    System.out.println(myStr.contains("Hel"));
+    System.out.println(myStr.contains("e"));
+    System.out.println(myStr.contains("Hi"));
+  }
+}
+
+
+//equals()
+public class Main {
+  public static void main(String[] args) {
+    String myStr1 = "Hell2o";
+    String myStr2 = "Hell2o";
+    String myStr3 = "Another String";
+    System.out.println(myStr1.equals(myStr2));  //true
+    System.out.println(myStr1.equals(myStr3));  //false
+  }
+}
+
+
+//
