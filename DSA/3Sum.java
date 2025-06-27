498a989559229c8405893c0b6c399c1f21cb8b0f
GarvGupta2005 <garvg310@gmail.com>
2025-06-28 04:30:21 +0530

    Update 3Sum.java

--- DSA/3Sum.java
+++ DSA/3Sum.java
@@ -3,20 +3,20 @@ Time Limit Exceeded
 /*
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();  // creates empty list [[]]
         for(int i = 0;i<nums.length; i++)
         {
             for(int j = 0;j<nums.length; j++)
             {
                 for(int k = 0;k<nums.length; k++)
                 {
-                    if((i!=j & i!=k & j!=k))
+                    if((i!=j && i!=k && j!=k))
                     {
                         if(nums[i]+nums[j]+nums[k]==0)
                         {
-                            List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                            List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);  // triplet here is a list of nums[i], nums[j], nums[k]
                             Collections.sort(triplet);
-                            if (!result.contains(triplet)) {
+                            if (!result.contains(triplet)) {  // if triplets are not in result
                                 result.add(triplet);
                             }
 
@@ -26,6 +26,9 @@ class Solution {
             }
         }
         return result;
+
+    }
+}
 
     }
 }
