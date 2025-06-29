7c4a5eb98903a8e6e4ea3e7116dcc1d7802e6566
GarvGupta2005 <garvg310@gmail.com>
2025-06-30 01:21:02 +0530

    Update My_code.java

--- DSA/169. Majority Element/My_code.java
+++ DSA/169. Majority Element/My_code.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int majorityElement(int[] arr) 
-    {
-        int res = 0;
-        for(int i = 0; i<arr.length; i++)
-        {
+    public int majorityElement(int[] arr) {
+        
+        int n = arr.length;
+        
+        for (int i = 0; i < n; i++) {
             int count = 0;
-            for(int j = 0; j<arr.length; j++)
-            {
-                if(arr[i]==arr[j])
-                {
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
                     count++;
                 }
             }
-            if(arr.length/2<count)
-            {
-                res = arr[i];
+            if (count > n / 2) {
+                return arr[i];  
             }
         }
-        return res;
+        return -1; 
     }
 }
