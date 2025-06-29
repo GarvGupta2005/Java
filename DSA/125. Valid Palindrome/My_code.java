afeb3773b7a1d6e6afb073ecfe68b965af633c58
GarvGupta2005 <garvg310@gmail.com>
2025-06-30 00:56:46 +0530

    Create My_code.java

---
+++ DSA/125. Valid Palindrome/My_code.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        String palindrome = "";
+        String lower_case = s.toLowerCase().replaceAll("[\\W_]", "");  // non-word characters and underscore
+
+       
+        for (int i = lower_case.length() - 1; i >= 0; i--) {
+            palindrome = palindrome + lower_case.charAt(i); 
+        }
+        if (palindrome.equals(lower_case)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
