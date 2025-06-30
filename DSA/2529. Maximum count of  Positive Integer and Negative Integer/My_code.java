60894e10bdc4a6a84fb0ae013aba1b4873e452c9
GarvGupta2005 <garvg310@gmail.com>
2025-07-01 01:40:46 +0530

    Create My_code.java

---
+++ DSA/2529. Maximum count of  Positive Integer and Negative Integer/My_code.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int neg = 0;
+        int pos = 0;
+
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(nums[i]<0)
+            {
+                neg++;
+            }
+            else
+            {
+                if(nums[i]>0)
+                {
+                    pos++;
+                }
+            }
+        }
+        if(neg>pos)
+        {
+            return neg;
+        }
+        else
+        {
+            return pos;
+        }
+    }
+}
