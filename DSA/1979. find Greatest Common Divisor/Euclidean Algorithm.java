f47fc2b39bfafc13fc20aa0a2449e03b20311a60
GarvGupta2005 <garvg310@gmail.com>
2025-07-02 19:12:07 +0530

    Create Euclidean Algorithm.java

---
+++ DSA/1979. find Greatest Common Divisor/Euclidean Algorithm.java
@@ -0,0 +1,20 @@
+// Fastest
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = nums[0], max = nums[0];
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        return gcd(min, max);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
+    }
+}
