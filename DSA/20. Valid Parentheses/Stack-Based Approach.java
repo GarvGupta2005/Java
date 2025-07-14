f89d55d545019f09341c610a0cd045eaa7e8c245
GarvGupta2005 <garvg310@gmail.com>
2025-07-14 21:37:11 +0530

    Update Stack-Based Approach.java

--- DSA/20. Valid Parentheses/Stack-Based Approach.java
+++ DSA/20. Valid Parentheses/Stack-Based Approach.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
-        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
-            s = s.replace("()", "");
-            s = s.replace("{}", "");
-            s = s.replace("[]", "");
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else {
+                if (stack.isEmpty() || stack.pop() != c) return false;
+            }
         }
-        return s.isEmpty();
+
+        return stack.isEmpty();
     }
 }
