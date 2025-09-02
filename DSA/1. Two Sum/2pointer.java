32ec8f5388e28d02ff269c92411dd8e095193a0a
GarvGupta2005 <garvg310@gmail.com>
2025-09-02 18:07:32 +0530

    Create 2pointer.java

---
+++ DSA/1. Two Sum/2pointer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end)
+        {
+            if(nums[start] + nums[end] > target)
+            {
+                end--;
+            }
+            else if(nums[start] + nums[end] < target)
+            {
+                start++;
+            }
+            else
+            {
+                return new int[]{start, end};
+            }
+        }
+        return new int[]{};
+    }    
+}
